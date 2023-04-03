29a00de975f68c6304ddde5206a50359b6ee9f48
Vinicius Bortolo <luizviniciusbortolo@gmail.com>
2023-04-03 10:24:06 -0300

    feat: import global styles, theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { ShopContextProvider } from './contexts/ShopContext'
+import { Router } from './Router'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 export function App() {
-  return <>App</>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <ShopContextProvider>
+          <Router />
+          <ToastContainer />
+        </ShopContextProvider>
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
